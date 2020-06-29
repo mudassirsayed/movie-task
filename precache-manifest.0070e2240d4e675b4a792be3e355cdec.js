d12187d8e5027e7a1029b0d7a47493f59cb23b78
Mudassir Sayed <sayedmudassir37@gmail.com>
2020-06-29 18:56:37 +0530

    Updates

---
+++ precache-manifest.0070e2240d4e675b4a792be3e355cdec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52707a6723227feeb26bba32bfddacd1",
+    "url": "/movie-task/index.html"
+  },
+  {
+    "revision": "35861767acdf98e624e4",
+    "url": "/movie-task/static/css/main.23223657.chunk.css"
+  },
+  {
+    "revision": "11346505b197ab60ff47",
+    "url": "/movie-task/static/js/2.f5a5dd95.chunk.js"
+  },
+  {
+    "revision": "971b4e562d3abe5fe0995509ffca7edb",
+    "url": "/movie-task/static/js/2.f5a5dd95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35861767acdf98e624e4",
+    "url": "/movie-task/static/js/main.665c5b04.chunk.js"
+  },
+  {
+    "revision": "63e3cfd01516e18c452a",
+    "url": "/movie-task/static/js/runtime-main.42f07757.js"
+  }
+]);
